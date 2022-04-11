7693e683cc70eadbc7960ce6b49f2497511531b0
nadysaptra <saputranady@gmail.com>
2022-04-11 13:46:22 +0700

    feat: add predefined component

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Contact from './components/Contact/Contact';
+import Experience from './components/Experience/Experience';
+import Footer from './components/Footer/Footer';
+import Intro from './components/Intro/Intro';
+import Navbar from './components/Navbar/Navbar';
+import Portfolio from './components/Portfolio/Portfolio';
+import Services from './components/Services/Services';
+import Works from './components/Works/Works';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
